b3083ea6eeb4fa5ad72f6e3350e3920eca5bcfcd
ks-77 <deka0987654@gmail.com>
2024-01-08 01:35:49 +0200

    second-task completed

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 HW3
 Savchenko Kirill
 """
-
+# 1
 try:
     print("1.Monday\n2.Tuesday\n3.Wednesday\n4.Thursday\n5.Friday\n6.Saturday\n7.Sunday")
     user_select = int(input("choose number between 1-7: "))
@@ -28,3 +28,26 @@ except ValueError as e:
     print(f"Error: {e}")
 except Exception as e:
     print(f"Error: {e}")
+
+# 2
+
+try:
+    first_number = int(input("Enter first number: "))
+    second_number = int(input("Enter second number: "))
+    if first_number == second_number:
+        print("congratulations, numbers are equal")
+    elif first_number != second_number:
+        if first_number <= second_number:
+            print(first_number, second_number)
+        elif first_number >= second_number:
+            print(second_number, first_number)
+        else:
+            print("Error")
+    else:
+        print("Error")
+
+except ValueError as e:
+    print("Enter only integer numbers")
+    print(f"Error {e}")
+except Exception as e:
+    print(f"Error {e}")
